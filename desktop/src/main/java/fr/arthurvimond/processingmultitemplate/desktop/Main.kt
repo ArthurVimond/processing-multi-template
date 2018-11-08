34e7020eb425b7731c5be0d278cf2f02d5ad111b
Arthur Vimond <arthurvimond@gmail.com>
2018-11-08 12:17:43 +0100

    feat(desktop): create independant desktop module

---
+++ desktop/src/main/java/fr/arthurvimond/processingmultitemplate/desktop/Main.kt
@@ -0,0 +1,8 @@
+package fr.arthurvimond.processingmultitemplate.desktop
+
+import fr.arthurvimond.processingmultitemplate.sketch.Sketch
+import processing.core.PApplet
+
+fun main(args: Array<String>) {
+    PApplet.main(Sketch::class.java.name, args)
+}
--- sketch/src/main/java/fr/arthurvimond/processingmultitemplate/sketch/Main.kt
+++
@@ -1,7 +0,0 @@
-package fr.arthurvimond.processingmultitemplate.sketch
-
-import processing.core.PApplet
-
-fun main(args: Array<String>) {
-    PApplet.main(Sketch::class.java.name, args)
-}
